b227c7e63a5b9ad79e81d653631567e3a0f44e74
Alex Titakoff <titakovad@webinnovations.ru>
2017-11-10 16:17:35 +0300

    hash generating

--- block.py
+++ block.py
@@ -1,20 +1,41 @@
+# coding=utf-8
 import json
+import os
+import hashlib
 
 
-def write_block():
+def get_hash(filename):
+    blockchain_dir = os.curdir + '/blockchain/'
+    file = open(blockchain_dir + filename, 'rb').read()
+    return hashlib.md5(file).hexdigest()
+
+
+def write_block(name, amount, to_whom, prev_hash=''):
+    blockchain_dir = os.curdir + '/blockchain/'
+
+    files = os.listdir(blockchain_dir)
+    files = sorted([int(i) for i in files])
+
+    last_file = files[-1]  # последний записанный блок
+
+    filename = str(last_file + 1)
+
+    prev_hash = get_hash(str(last_file))
+
     data = {
-        'name': 'alex ti',
-        'amount': 5,
-        'to_whom': 'vasja',
-        'hash': '123'
+        'name': name,
+        'amount': amount,
+        'to_whom': to_whom,
+        'hash': prev_hash
     }
 
-    with open('test', 'w') as file:
+    with open(blockchain_dir + filename, 'w') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
 
 
 def main():
-    write_block()
+    write_block('alex titakob', 4, 'kate')
+
 
 if __name__ == '__main__':
     main()
