a9cb5cc124ef516bb76a1757eeb1b6c325700f8a
Alex Titakoff <titakovad@webinnovations.ru>
2017-11-10 17:19:27 +0300

    Any refactor / part before web flask client

--- block.py
+++ block.py
@@ -3,27 +3,30 @@ import json
 import os
 import hashlib
 
+blockchain_dir = os.curdir + '/blockchain/'
+
 
 def get_hash(filename):
-    blockchain_dir = os.curdir + '/blockchain/'
     file = open(blockchain_dir + filename, 'rb').read()
     return hashlib.md5(file).hexdigest()
 
+
+def get_files():
+    files = os.listdir(blockchain_dir)
+    return sorted([int(i) for i in files])
 
 def check_integrity():
     # считать хэш предыдущего блока
     # вычислить хэш
     # сравнить полученные данные
-    blockchain_dir = os.curdir + '/blockchain/'
-    files = os.listdir(blockchain_dir)
-    files = sorted([int(i) for i in files])
+    files = get_files()
+
+    result = []
 
     for file in files[1:]:
         f = open(blockchain_dir + str(file))
         h = json.load(f)['hash']
-
         prev_file = str(file - 1)
-
         actual_hash = get_hash(prev_file)
 
         if h == actual_hash:
@@ -33,19 +36,16 @@ def check_integrity():
 
         print('block {} is {}'.format(prev_file, res))
 
+        result.append({'block': prev_file,'result': res})
+
+    return result
+
 
 def write_block(name, amount, to_whom, prev_hash=''):
-    blockchain_dir = os.curdir + '/blockchain/'
-
-    files = os.listdir(blockchain_dir)
-    files = sorted([int(i) for i in files])
-
+    files = get_files()
     last_file = files[-1]  # последний записанный блок
-
     filename = str(last_file + 1)
-
     prev_hash = get_hash(str(last_file))
-
     data = {
         'name': name,
         'amount': amount,
@@ -58,7 +58,7 @@ def write_block(name, amount, to_whom, prev_hash=''):
 
 
 def main():
-    write_block('alex titakob', 4, 'kate')
+    #write_block('alex titakob', 4, 'kate')
     check_integrity()
 
 
