bf7382fdef6dd56f65057239832f8b092b0f3a26
Alex Titakoff <titakovad@webinnovations.ru>
2017-11-10 14:14:48 +0300

    init pyton blockchain proj

---
+++ block.py
@@ -0,0 +1,20 @@
+import json
+
+
+def write_block():
+    data = {
+        'name': 'ivan',
+        'amount': 5,
+        'to_whom': 'vasja',
+        'hash': '123'
+    }
+
+    with open('test', 'w') as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
+
+
+def main():
+    write_block()
+
+if __name__ == '__main__':
+    main()
